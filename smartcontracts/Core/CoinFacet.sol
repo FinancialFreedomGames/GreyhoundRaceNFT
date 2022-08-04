776a5b9d8bccad47776f7f6adb1b4b6024e3d245
AntoU <centerbot0001@gmail.com>
2022-08-04 16:48:05 +0200

    Facet to manage token's address of all game

---
+++ smartcontracts/Core/CoinFacet.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import { GreyhoundRace } from "../Libraries/GreyhoundRaceDiamondStorage.sol";
+contract CoinFacet {
+    function setCoin(address _address) external {
+        GreyhoundRace.whenPermited();
+        GreyhoundRace.DiamondStorage storage ds = GreyhoundRace.diamondStorage();
+        ds.coin=_address;
+    }
+    function initialize() external{
+        GreyhoundRace.whenPermited();
+        GreyhoundRace.DiamondStorage storage ds = GreyhoundRace.diamondStorage();
+        ds.coin=0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;//USDC address
+    }
+    function getSelectors() external pure returns(bytes4[] memory selector){
+        selector=new bytes4[](1);
+        selector[0]=this.setCoin.selector;
+    }
+}
