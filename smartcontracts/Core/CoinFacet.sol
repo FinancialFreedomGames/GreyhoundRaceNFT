5fdf82225fc382d10dc35369fc9cb1b1b3ec17cf
traycon <centerbot0001@gmail.com>
2022-09-09 19:28:02 +0200

    Implemented getCoin

--- smartcontracts/Core/CoinFacet.sol
+++ smartcontracts/Core/CoinFacet.sol
@@ -8,13 +8,17 @@ contract CoinFacet {
         GreyhoundRace.DiamondStorage storage ds = GreyhoundRace.diamondStorage();
         ds.coin=_address;
     }
-    function initialize() external{
+    function getCoin() external view returns(address coin) {
+        coin=GreyhoundRace.coinAddress();
+    }
+    function initialize() external {
         GreyhoundRace.whenPermited();
         GreyhoundRace.DiamondStorage storage ds = GreyhoundRace.diamondStorage();
         ds.coin=0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;//USDC address
     }
-    function getSelectors() external pure returns(bytes4[] memory selector){
-        selector=new bytes4[](1);
+    function getSelectors() external pure returns(bytes4[] memory selector) {
+        selector=new bytes4[](2);
         selector[0]=this.setCoin.selector;
+        selector[1]=this.getCoin.selector;
     }
 }
